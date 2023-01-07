07a1c18ef9fca57c648ac7050277085c2036cf32
yujiniii <youjin030700@gmail.com>
2023-01-07 19:06:59 +0900

    fix: db 모델 추가
    N:M 에서 through로 지정된 모델에 접근하기 위해 별도로 모델 추가함

---
+++ models/dotToLine.model.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+const DotToLine = (sequelize) => sequelize.define('DotToLines',{
+    dotToLine_id : {
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    dot_id : {
+        type:DataTypes.INTEGER,
+        primaryKey:true
+    },
+    line_id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true
+    }
+},{
+    timestamps: false
+});
+
+module.exports = DotToLine;
