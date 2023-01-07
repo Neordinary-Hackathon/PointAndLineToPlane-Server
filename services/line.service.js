3e87fe6997bd4c1233f1e3a96e22d4b5fc2305de
yujiniii <youjin030700@gmail.com>
2023-01-08 02:57:26 +0900

    fix: 사용자 정보를 불러오는 방법 변경
    블록단위 스코프로 인해 axios로 불러온 값이 남지 않고 사라져버려서 어쩔 수 없이 그냥 그때그때 불러와서 then()으로 감싸버림

--- services/line.service.js
+++ services/line.service.js
@@ -1,17 +1,32 @@
-const {Line,Dot,DotToLine} = require("../models");
-const kakaoService = require('./kakao.service.js');
+const {Line,Dot,DotToLine, User} = require("../models");
+const axios = require("axios");
 
 const findMyDotContent = async (req,res,next)=>{
     let token = req.headers['authorization'];
     token = token.replace(/^Bearer\s+/, "");
-    const user = await kakaoService.findUserPk(token) // 카카오에서 aixos로 사용자 정보 가져오기
-    const dots = await Dot.findAll({where:{
-        user_id:user
-    }});
-    if (dots.length === 0 || !dots){
-        return res.status(400).json({"message": "단어를 먼저 등록해주세요"});
-    }
-    return res.status(200).json({"dots": dots});
+    await axios({
+        method:'get',
+        url:'https://kapi.kakao.com/v1/user/access_token_info',
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }).then(async (result)=>{
+        const resultId = String(result.data.id)
+        const isUser = await User.findOne({
+            where:{
+                unique_id:resultId
+            }
+        })
+        if(!isUser || typeof isUser === 'undefined'){}
+        const dots = await Dot.findAll({where:{
+                user_id:isUser.user_id
+            }});
+        if (dots.length === 0 || !dots){
+            return res.status(400).json({"message": "단어를 먼저 등록해주세요"});
+        }
+        return res.status(200).json({"dots": dots});
+    })
+
 }
 
 
@@ -20,21 +35,33 @@ const saveLineContent = async (req, res, next)=>{
     const dot_id_list = dot_id.split(' ')
     let token = req.headers['authorization'];
     token = token.replace(/^Bearer\s+/, "");
-    const user = await kakaoService.findUserPk(token) // 카카오에서 aixos로 사용자 정보 가져오기
-
-    const newLine = await Line.create({
-        user_id:user,
-        line_content:line_content
-    })
-    for(let idx in dot_id_list){
-        await DotToLine.create({
-            line_id : newLine.line_id,
-            dot_id:Number(dot_id_list[idx])
+    await axios({
+        method:'get',
+        url:'https://kapi.kakao.com/v1/user/access_token_info',
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }).then(async (result)=>{
+        const resultId = String(result.data.id)
+        const isUser = await User.findOne({
+            where:{
+                unique_id:resultId
+            }
         })
-    }
+        if(!isUser || typeof isUser === 'undefined'){}
+        const newLine = await Line.create({
+            user_id:isUser.user_id,
+            line_content:line_content
+        })
+        for(let idx in dot_id_list){
+            await DotToLine.create({
+                line_id : newLine.line_id,
+                dot_id:Number(dot_id_list[idx])
+            })
+        }
 
-    return res.status(200).json({"message": newLine});
-
+        return res.status(200).json({"message": newLine});
+    })
 }
 
 module.exports = {
