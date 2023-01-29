8eeb3d7cd9c6d7b0e259ab6220bdad3e57e4fa32
yujiniii <50348197+yujiniii@users.noreply.github.com>
2023-01-30 00:23:37 +0900

    HOT FIX

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const kakaoService = require('../../services/kakao.service');
 const userService = require('../../services/user.service');
-
+const authenticate = require('../middleware/auth.middleware')
 router.post('/auth', (req, res, next) => {
   kakaoService.getToken(req, res, next);
 });
@@ -10,10 +10,10 @@ router.post('/register', (req, res, next) => {
   kakaoService.loginOrRegisterOrFindPk(req, res, next);
 });
 
-router.get('/my-page', (req, res, next) => {
+router.get('/my-page', authenticate,(req, res, next) => {
   return userService.viewMyInfo(req, res, next);
 });
-router.post('/my-page', (req, res, next) => {
+router.post('/my-page', authenticate,(req, res, next) => {
   return userService.updateMyInfo(req, res, next);
 });
 
